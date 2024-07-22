5eec180511e0cf2f38d722cfeeafde762a55fb24
Yatharth-14 <yatharthyadav123@gmail.com>
2024-07-22 09:55:37 +0530

    first commit of the day

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -1,16 +1,22 @@
 // import React from 'react'
+// import { Link } from "react-router-dom";
 import { Button } from "../ui/button";
 
 const Header = () => {
   return (
-    <>
-      <div className="px-5 flex justify-between items-center">
-        <img src="/logo2.svg" alt="logo" height="90px" width="90px" className="cursor-pointer" />
-        <div>
-          <Button>Sign In</Button>
-        </div>
+    <div className="px-5 flex justify-between items-center">
+      <img
+        src="/logo2.svg"
+        alt="logo"
+        height="90px"
+        width="90px"
+        className="cursor-pointer"
+      />
+
+      <div>
+        <Button>Sign In</Button>
       </div>
-    </>
+    </div>
   );
 };
 
